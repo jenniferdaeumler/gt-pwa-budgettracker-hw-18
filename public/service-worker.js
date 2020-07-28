d8c68eadddd9d7ca935ce28d93f9b3f0a813b90a
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-28 10:53:43 -0500

    fixed service worker problem.  copied DB from jon's code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,46 @@
-// console.log("Service worker loaded");
-
-// Uncomment the lines below
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "favicon.ico",
-  "index.js",
-  "manifest.webmanifest",
-  "service-worker.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-  "style.css",
-];
+    "/",
+    "/index.html",
+    "db.js",
+    "favicon.ico",
+    "index.js",
+    "manifest.webmanifest",
+    "service-worker.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/styles.css"
+  ];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        cache
+          .addAll(FILES_TO_CACHE)
+          .then((result) => {
+            // debugger;
+            console.log("result of add all", result);
+          })
+          .catch((err) => {
+            // debugger;
+            console.log("Add all error: ", err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   );
 
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -44,8 +58,33 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", function (evt) {
-  // code to handle requests goes here
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
   evt.respondWith(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.match(evt.request).then((response) => {
